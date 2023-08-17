9ec206bbe5224fd69444de44635f9f8fb89ba5a9
leon <leon.liu@accuhit.net>
2023-08-17 17:17:35 +0800

    異步引入的 js 打包

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
-import { obj1 } from './js/someExport'
-console.log(obj1)
+// import { obj1 } from './js/someExport'
+// console.log(obj1)
+
+async function importSomething() {
+  await setTimeout(() => {
+    const { obj1 } = import('./js/someExport');
+  }, 1000)
+}
+
+importSomething()
+
+
+// import pic from './assets/img/pic.jpg'
--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,9 @@ module.exports = {
   entry: ['./src/main'], // 在 index 檔案後的 .js 副檔名是可選的
   output: {
     path: path.join(__dirname, 'dist'),
-    filename: '[name]_[hash].js'
+    asyncChunks: true,
+    filename: '[name]_[hash].js',
+    chunkFilename: 'chunk_[hash].js',
+    assetModuleFilename: '[name]_[hash]'
   }
 }
